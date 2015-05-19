a0669fbb59992fa70d302d6d3c4ec0adc0bad6ec
walid belkziz <b.walid1@gmail.com>
2015-05-19 18:30:36 +0100

    Adding static files

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,8 @@ var jsSources = [
 	'components/scripts/template.js'
 ]
 var sassSources = ['components/sass/style.scss'];
+var htmlSources = ['build/Dev/*.html'];
+var jsonSources = ['build/Dev/js/*.json'];
 
 
 gulp.task('coffee', function () {
@@ -47,6 +49,8 @@ gulp.task('watch', function() {
 	gulp.watch(coffeeSources, ['coffee'])
 	gulp.watch(jsSources, ['js'])
 	gulp.watch('components/sass/*.scss', ['compass'])
+	gulp.watch(htmlSources, ['html'])
+	gulp.watch('build/Dev/js/*.json', ['js'])
 })
 
 gulp.task('connect', function() {
@@ -56,4 +60,13 @@ gulp.task('connect', function() {
 	})
 })
 
-gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
+gulp.task('html', function() {
+	gulp.src(htmlSources)
+		.pipe(connect.reload())
+})
+gulp.task('json', function() {
+	gulp.src(jsonSources)
+		.pipe(connect.reload())
+})
+
+gulp.task('default', ['html', 'json','coffee', 'js', 'compass', 'connect', 'watch']);
